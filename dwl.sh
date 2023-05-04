5d44860aebd090a78f61a7afbedb347f2facfee9
TandemK <105143974+TandemK@users.noreply.github.com>
2023-05-04 11:48:37 +0300

    Update dwl.sh

--- dwl.sh
+++ dwl.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+echo Start
 date | tee  script.log
-NET=testnet
+NET=$NETWORK
 #NET=mainnet
+#NET=testnet
+
 aws s3 --no-sign-request cp s3://near-protocol-public/backups/$NET/rpc/latest .
 LATEST=$(cat latest)
-echo $LATEST
+echo "Snapshot in the $NET network - $LATEST"
 
-aws s3 cp --no-sign-request --recursive s3://near-protocol-public/backups/$NET/rpc/$LATEST ./data_$NET\_$LATEST
+echo "Start download..."
 aws s3 sync --no-sign-request  s3://near-protocol-public/backups/$NET/rpc/$LATEST ./data_$NET\_$LATEST
 date | tee -a script.log
